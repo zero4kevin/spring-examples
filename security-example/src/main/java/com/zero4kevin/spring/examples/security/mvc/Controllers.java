c89a4441847b3fa5d1407bbbb6b167f0e7c58e48
zero4kevin <kevin21th@hotmail.com>
2018-02-04 23:11:39 +0800

    add the modules into the project, and add a security example with mvc

---
+++ security-example/src/main/java/com/zero4kevin/spring/examples/security/mvc/Controllers.java
@@ -0,0 +1,15 @@
+package com.zero4kevin.spring.examples.security.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by kevin on 2/3/18.
+ */
+@Controller
+public class Controllers {
+    @RequestMapping({"/","/home"})
+    public String welcome(){
+        return "home";
+    }
+}
