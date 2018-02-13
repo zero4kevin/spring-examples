0baa54c909a175862e3aed3b9259d10aebcbbb71
xi1zhang <kevin.zhang@nokia-sbell.com>
2018-02-13 14:20:22 +0800

    add the example of registering a new user

---
+++ security-example/src/main/java/com/zero4kevin/spring/examples/security/DTO/UserDTO.java
@@ -0,0 +1,66 @@
+package com.zero4kevin.spring.examples.security.DTO;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by xi1zhang on 2018/2/13.
+ */
+public class UserDTO {
+    @NotNull
+    @NotEmpty
+    private String firstName;
+
+    @NotNull
+    @NotEmpty
+    private String lastName;
+
+    @NotNull
+    @NotEmpty
+    private String password;
+    private String confirmPassword;
+
+    @NotNull
+    @NotEmpty
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- security-example/src/main/java/com/zero4kevin/spring/examples/security/mvc/Controllers.java
+++ security-example/src/main/java/com/zero4kevin/spring/examples/security/mvc/Controllers.java
@@ -1,7 +1,17 @@
 package com.zero4kevin.spring.examples.security.mvc;
 
+import com.zero4kevin.spring.examples.security.DTO.UserDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 /**
  * Created by kevin on 2/3/18.
@@ -16,5 +26,18 @@ public class Controllers {
     @RequestMapping("/member")
     public String memberPage(){
         return "home";
+    }
+
+    @RequestMapping(value="/user/registration", method=RequestMethod.GET)
+    public String showRegistrationForm(WebRequest request, Model model){
+        UserDTO user= new UserDTO();
+        model.addAttribute("user", user);
+        return "registration";
+    }
+
+    public ModelAndView registerUserAccount(
+            @ModelAttribute("user") @Valid UserDTO accountDto,
+            BindingResult result, WebRequest request, Errors errors) {
+        return new ModelAndView();
     }
 }
